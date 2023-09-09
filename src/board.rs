3e2faef13dfe53215094df1e8e36f7856b14d6f3
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2023-09-09 12:50:53 +0200

    Board positions and start of board code

---
+++ src/board.rs
@@ -0,0 +1,74 @@
+use crate::pos::BoardPos;
+
+const BOARD_SIZE: usize = 8;
+
+pub struct Board {
+    data: [[Option<Tile>; BOARD_SIZE]; BOARD_SIZE],
+}
+
+impl Board {
+    pub fn empty() -> Board {
+        Board {
+            data: [[None; BOARD_SIZE]; BOARD_SIZE]
+        }
+    }
+
+    pub fn get_tile(&self, pos: &BoardPos) -> Option<Tile> {
+        self.data[pos.rank() as usize][pos.file() as usize]
+    }
+
+    pub fn set_tile(&mut self, pos: &BoardPos, tile: Tile) {
+        self.data[pos.rank() as usize][pos.file() as usize] = Some(tile);
+    }
+}
+
+/// A tile on the chess board, for example a black king or a white knight.
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct Tile {
+    piece: PieceType,
+    color: Color,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Color {
+    White,
+    Black,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum PieceType {
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn,
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_empty_board() {
+        let board = Board::empty();
+
+        let pos = "b2".parse().unwrap();
+        assert!(board.get_tile(&pos).is_none());
+    }
+
+    #[test]
+    fn set_get_tiles() {
+        let mut board = Board::empty();
+
+        let pos = "b2".parse().unwrap();
+        let tile1 = Tile { piece: PieceType::King, color: Color::White };
+
+        board.set_tile(&pos, tile1.clone());
+
+        let tile2 = board.get_tile(&pos);
+
+        assert_eq!(tile1, tile2.unwrap());
+    }
+}
---
+++ src/pos.rs
@@ -0,0 +1,137 @@
+use std::fmt;
+
+/// A struct that represends valid positions on a chess board.
+/// 
+/// The rank is a number between `[0-7]` (inclusive) where `0` represents the
+/// first rank, commonly displayed as rank 1 and where `7` is the last rank,
+/// commonly displayed as rank 8.
+/// 
+/// The file is a number between `[0-7]` (inclusive) where `0` represents the
+/// file `a` and `7` represents the file `h`.
+/// 
+/// ## Example
+/// The position `b4` has a rank value `3` and a file value `1`.
+#[derive(PartialEq, Eq, Debug)]
+pub struct BoardPos {
+    rank: u8,
+    file: u8,
+}
+
+impl BoardPos {
+    /// Create a BoardPos instance.
+    /// 
+    /// ## Panics
+    /// This function will panic if either the rank or file is outside of the
+    /// inclusive range `[0, 7]`.
+    pub fn new(rank: u8, file: u8) -> BoardPos {
+        if rank > 7 {
+            panic!("rank must be in the inclusive range [0-7], got {}", rank);
+        }
+        if file > 7 {
+            panic!("file must be in the inclusive range [0-7], got {}", file);
+        }
+        BoardPos { rank, file }
+    }
+
+    /// Get the internal representation of the rank as an integer between `[0-7]`.
+    /// 
+    /// This is not the human-readable rank.
+    pub fn rank(&self) -> u8 {
+        self.rank
+    }
+
+    /// Get the internal representation of the file as an integer between `[0-7]`.
+    /// 
+    /// This is not the human-readable file.
+    pub fn file(&self) -> u8 {
+        self.file
+    }
+
+    /// Get the lowercase character for the file represented by this board position.
+    pub fn file_char(&self) -> char {
+        // 0 -> a, 1 -> b, ..., 7 -> h
+        ('a' as u8 + self.file as u8) as char
+    }
+
+}
+
+impl fmt::Display for BoardPos {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.file_char(), self.rank + 1)
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseBoardPosError {
+    msg: &'static str,
+}
+
+impl std::str::FromStr for BoardPos {
+    type Err = ParseBoardPosError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+
+        let file_char = chars.next()
+            .ok_or(ParseBoardPosError { msg: "String too short." })?;
+
+        let rank = chars.next()
+            .ok_or(ParseBoardPosError { msg: "String too short." })?;
+
+        if !chars.next().is_none() {
+            return Err(ParseBoardPosError { msg: "String too long." });
+        }
+
+        let rank = rank.to_digit(10)
+            .ok_or(ParseBoardPosError { msg: "Second character must be a digit." })? as u8;
+
+        let rank = rank - 1;
+
+        let file = file_char as u8 - 'a' as u8;
+
+        Ok(BoardPos { rank, file })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_board_pos() {
+        let pos = BoardPos::new(3, 7);
+        
+        assert_eq!(pos.rank(), 3);
+        assert_eq!(pos.file(), 7);
+        assert_eq!(pos.file_char(), 'h');
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_board_pos_file() {
+        BoardPos::new(2, 8);
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_board_pos_rank() {
+        BoardPos::new(8, 2);
+    }
+
+    #[test]
+    fn format_board_pos() {
+        let pos = BoardPos::new(3, 1);
+
+        assert_eq!(pos.to_string(), "b4");
+    }
+
+    #[test]
+    fn parse_board_pos() {
+        let pos1: BoardPos = "b4".parse().unwrap();
+        let pos2 = BoardPos::new(3, 1);
+
+        assert_eq!(pos1, pos2);
+        assert_eq!(pos1.to_string(), "b4");
+    }
+}
